ee076aa3dc4f345d9acf1cb409ddc1ef23bc2836
it <2824397386@qq.com>
2021-09-25 11:05:37 +0800

    线程

--- 4.py
+++ 4.py
@@ -5,7 +5,7 @@ basket = 0
 
 class A(Thread):
     cook_name = ''
-    basket_count = 1
+    basket_count = 50
     def run(self) -> None:
         global basket
         print(self.cook_name,'正在做蛋挞')
@@ -18,18 +18,30 @@ class B(Thread):
     customer_name = ''
     customer_money = 3000
     eggshell = 2
-    eggshell_count = 1
+    eggshell_count = 30
     def run(self) -> None:
         global basket
-        print(self.customer_name,'正在买蛋挞')
-        if basket == 0:
-            time.sleep(2)
-        else:
-            self.customer_money = self.customer_money - self.eggshell* self.eggshell_count
-a = A()
-b = B()
-a.cook_name = '张三'
-b.customer_name = '老大'
+        while True:
+            print(self.customer_name,'正在买蛋挞')
+            if basket == 0:
+                time.sleep(2)
+            else:
+                self.customer_money = self.customer_money - self.eggshell* self.eggshell_count
+            if self.customer_money == 0:
+                break
 
-a.start()
-b.start()
+a1 = A()
+b1 = B()
+a1.cook_name = '张三'
+b1.customer_name = '老大'
+
+a1.start()
+b1.start()
+
+a2 = A()
+b2 = B()
+a2.cook_name = '李四'
+b2.customer_name = '老二'
+
+a2.start()
+b2.start()
